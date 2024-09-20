ffb9382b41c90b4ecd9697e94c408a2c28551e73
Gulu <penaddlight@outlook.com>
2024-09-20 23:48:31 +0800

    level 1-3(not finished yet)

--- VendingMachine.c
+++ VendingMachine.c
@@ -3,7 +3,9 @@
 int main()
 {
     int aisle, price, num, total_price;
+    int w_aisle, w_num;
     char type;
+    char w_type;
 
     printf("Please enter the type,asile,price,and number in order:");
     scanf("%c%d%d%d", &type, &aisle, &price, &num);
@@ -15,16 +17,38 @@ int main()
     }
     printf(" %d\n", price);
 
-    printf("Total price:");
-    scanf("%d", &total_price);
+    while(getchar()!='\n')
+        continue;
+    
 
-    for ( int coin; total_price > 0; )
+    for( char cmd = 'y'; num > 0 && cmd == 'y'; )
     {
+        printf("The type,aisle,and number of the goods that you wanna buy:");
+        scanf("%c%d%d", &type, &w_aisle, &w_num);
+        total_price = w_num * price;
+        printf("Total price:%d\n", total_price);
+
+        for ( int coin; total_price > 0; )
+        {
         printf("Please insert coins here:");
         scanf("%d", &coin);
         total_price -= coin;
+        }
+        printf("Here is the change:%d\n", -total_price);
+
+        num -= w_num;
+
+        while(getchar()!='\n')
+            continue;
+        
+        printf("Wanna to purchase again?(y/n)");
+        scanf("%c", &cmd);
+
+        while(getchar()!='\n')
+            continue;
+        
+        
     }
-    printf("Here is the change:%d", -total_price);
 
     return 0;
 }
